b799a15edac943bc71a890eb6d0403c7f05ffc4d
axulina <zainullina.a@gmail.com>
2017-02-05 08:24:50 +1100

    values except birth date became optional. in this case, characteristics that change their values throughout the day, depending on location/time are returned as "vary"

--- lambda.py
+++ lambda.py
@@ -5,24 +5,46 @@ from flatlib.geopos import GeoPos
 
 
 def handle(event, context):
+    gimme_these = ['SUN', 'MOON', 'MERCURY', 'VENUS', 'MARS', 'JUPITER', 'SATURN', 'URANUS', 'NEPTUNE', 'PLUTO',
+                   'CHIRON', 'HOUSE1', 'HOUSE2', 'HOUSE3', 'HOUSE4', 'HOUSE5', 'HOUSE6', 'HOUSE7', 'HOUSE8', 'HOUSE9',
+                   'HOUSE10', 'HOUSE11', 'HOUSE12']
     date = str(event['date'])
-    time = str(event['time'])
-    timezone = str(event['timezone'])
-    lat = str(event['lat'])
-    lng = str(event['lng'])
-    pos = GeoPos(lat, lng)
-    datetime = Datetime(date, time, timezone)
-    chart = Chart(datetime, pos)
-    return [
-        {'characteristic': 'Sun', 'sign': chart.get(const.SUN).sign, 'angle': str(chart.get(const.SUN).signlon)},
-        {'characteristic': 'Moon', 'sign': chart.get(const.MOON).sign, 'angle': chart.get(const.MOON).signlon},
-        {'characteristic': 'Mercury', 'sign': chart.get(const.MERCURY).sign, 'angle': chart.get(const.MERCURY).signlon},
-        {'characteristic': 'Mars', 'sign': chart.get(const.MARS).sign, 'angle': chart.get(const.MARS).signlon},
-        {'characteristic': 'Venus', 'sign': chart.get(const.VENUS).sign, 'angle': chart.get(const.VENUS).signlon},
-        {'characteristic': 'Jupiter', 'sign': chart.get(const.JUPITER).sign, 'angle': chart.get(const.JUPITER).signlon},
-        {'characteristic': 'Saturn', 'sign': chart.get(const.SATURN).sign, 'angle': chart.get(const.SATURN).signlon},
-        # {'characteristic': 'Uranus', 'sign': chart.get(const.URANUS).sign, 'angle': chart.get(const.URANUS).signlon},
-        # {'characteristic': 'Neptune', 'sign': chart.get(const.NEPTUNE).sign, 'angle': chart.get(const.NEPTUNE).signlon},
-        {'characteristic': 'Ascendant', 'sign': chart.get(const.ASC).sign, 'angle': chart.get(const.ASC).signlon},
-    ]
+    time = str(event.get('time', ''))
+    timezone = str(event.get('timezone', ''))
+    lat = str(event.get('lat', '')).replace('.', ':')
+    lng = str(event.get('lng', '')).replace('.', ':')
 
+    # date = '1987/04/20'
+    # time = '15:30'
+    # timezone = '+01:00'
+    # lat = ''
+    # lng = ''
+
+    time_is_good = (time != '' and timezone != '')
+    place_is_good = (lat != '' and lng != '')
+    if time_is_good:
+        datetime = [Datetime(date, time, timezone)]
+    else:
+        datetime = [Datetime(date, "23:59", "-12:00"), Datetime(date, "00:01", "+12:00")]
+    if place_is_good:
+        pos = [GeoPos(lat, lng)]
+    else:
+        pos = [GeoPos('-60:000', '-180:000'), GeoPos('78:000', '180:000')]
+    ret = []
+    if len(datetime) * len(pos) == 1:
+        chart = Chart(datetime, pos, IDs=const.LIST_OBJECTS)
+        for item in gimme_these:
+            key = getattr(const, item)
+            ret.append({'characteristic': key, 'sign': chart.get(key).sign, 'angle': str(chart.get(key).signlon)})
+    else:
+        chart1 = Chart(datetime[0], pos[0], IDs=const.LIST_OBJECTS)
+        chart2 = Chart(datetime[-1], pos[-1], IDs=const.LIST_OBJECTS)
+        for item in gimme_these:
+            key = getattr(const, item)
+            is_same = (chart1.get(key).sign == chart2.get(key).sign)
+            ret.append({'characteristic': key,
+                        'sign': str(chart1.get(key).sign if is_same else "vary"),
+                        'angle': str(chart1.get(key).signlon if is_same else "vary")})
+    return ret
+
+print handle(1,2)
